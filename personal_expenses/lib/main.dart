347628897d8fd8c40d597bd4df4d963aaf6bac10
Leonildo Silva <leocsilva@gmail.com>
2019-11-26 16:48:34 -0300

    Personal Expenses App

---
+++ personal_expenses/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:personal_expenses/widgets/user_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Container(
+                child: Text('Chart!'),
+              ),
+              elevation: 5,
+            ),
+          ),
+          UserTransaction()
+        ],
+      ),
+    );
+  }
+}
